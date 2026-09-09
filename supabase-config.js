9f91f1e4e0d67fa529a82493d642ce222b14ee12
tawhidshifat1 <tawhid.shifat1@gmail.com>
2026-09-09 20:44:49 +0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-var SUPABASE_URL = 'https://wpfecefseyltahuwqrnw.supabase.co/rest/v1/';
+var SUPABASE_URL = 'https://wpfecefseyltahuwqrnw.supabase.co';
 var SUPABASE_ANON_KEY = 'sb_publishable_2Yu-KVpYeGdTevg5fE1bRg_pslPMvt5';
