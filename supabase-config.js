94ed4b1f8d8c6bff5d86b48a11921d1d0d448e67
tawhidshifat1 <tawhid.shifat1@gmail.com>
2026-09-09 20:25:42 +0600

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,2 @@
-/* Supabase কনফিগারেশন — শুধু এই ফাইলেই কী থাকে */
-var SUPABASE_URL = 'https://wpfcefseyltahuwqrnw.supabase.co';
+var SUPABASE_URL = 'https://wpfecefseyltahuwqrnw.supabase.co/rest/v1/';
 var SUPABASE_ANON_KEY = 'sb_publishable_2Yu-KVpYeGdTevg5fE1bRg_pslPMvt5';
